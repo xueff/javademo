ab8d5413d0c2b506c4445e975c10f143b51bcfdc
xueff <657413186@qq.com>
2018-10-23 10:12:37 +0800

    删除

--- src/test/java/test/Teat.java
+++ src/test/java/test/Teat.java
@@ -1,5 +1,11 @@
 package test;
 
+import common.bean.Person;
+import ramdon.RamdonStudy;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author xuefei
  * @Title: ${file_name}
@@ -8,4 +14,15 @@ package test;
  * @date 2018/10/239:15
  */
 public class Teat {
+    public static void main(String[] args) {
+        List<Person> pl = new ArrayList<>();
+        for(int i=0;i<5;i++){
+            Person p = new Person();
+            p.setName("A"+i);
+            p.setAge(RamdonStudy.getRamdonInt(10));
+            pl.add(p);
+        }
+
+
+    }
 }
