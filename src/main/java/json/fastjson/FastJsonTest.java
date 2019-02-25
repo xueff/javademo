b65b762bc3b90a6c6d862c6321aeab63aa8e23cf
xueff <657413186@qq.com>
2019-02-25 11:31:46 +0800

    fastjson

--- src/main/java/json/fastjson/FastJsonTest.java
+++ src/main/java/json/fastjson/FastJsonTest.java
@@ -1,26 +1,29 @@
 package json.fastjson;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import common.bean.Person;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+//https://www.w3cschool.cn/fastjson/fastjson-serializefilter.html
 public class FastJsonTest {
+    //TODO
+    //Fastjson 处理超大JSON文本
 
     public static void main(String[] args) {
+        ser();
+    }
+    //定制序列化名称，注解
+    //{"userName":"a","Age":18}
+    //Person{name='a', age=18, money=0.3530492034117335}
+    public static void ser() {
+        JSONObject json = new JSONObject();
+        json.put("userName","a");
+        json.put("Age",18);
+        System.out.println(json.toString());
+        Person p = JSON.toJavaObject(json,Person.class);
+        System.out.println(p.toString());
+    }
+    public static void baseSer() {
         JSONObject json = new JSONObject();
         json.put("a","a");
         System.out.println("json字符串:"+json.toJSONString());
@@ -28,14 +31,9 @@ public class FastJsonTest {
         json = JSONObject.parseObject(json.toJSONString());
         System.out.println("json字符串转json结果:"+json.toJSONString());
 
-
         Person student1 = new Person();
         student1.setName("hh");
         System.out.println("bean转json Str结果:"+JSONObject.toJSONString(student1));
         System.out.println("bean转jsonObj or jsonArray结果:"+JSONObject.toJSON(student1));
-
-
-
-
     }
 }
