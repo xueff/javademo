510584ca348f7e6b37abd03368686d112337eeb0
xueff <657413186@qq.com>
2019-04-09 10:49:20 +0800

    反射

---
+++ src/main/java/javabase/reflect/TestReflect.java
@@ -0,0 +1,44 @@
+package javabase.reflect;
+
+import org.junit.Test;
+
+/**
+ * java反射
+ */
+public class TestReflect {
+//    private String proprety = null;
+//    Dto dto = new Dto();
+//    @Test
+//    public void a() throws Exception {
+//        Class<?> clazz = Class.forName("mytest.test.Dto");
+//        Object obj = clazz.newInstance();
+//        // 可以直接对 private 的属性赋值
+//        Field field = clazz.getDeclaredField("id");
+//        field.setAccessible(true);
+//        field.set(obj, "Java反射机制");
+//        System.out.println(field.get(obj));
+//    }
+//
+//    @Test
+//    public void test() throws Exception {
+//        Dog dog = new Dog(1,"ww",1);
+//        Cat cat = new Cat(1,"mm",1);
+//        convert((Object)dog,0);
+//        convert((Object)cat,1);
+//    }
+//
+//    private Animal convert(Object obj,int type) {
+//        Class<?> classType2 = obj.getClass();
+//        System.out.println(classType2);
+//
+//        Dog dog = null;
+//        Cat cat = null;
+//
+//        Animal an = new Animal();
+//        dog =(Dog)obj;
+//        an.setId(dog.getId());
+//
+//        return null;
+//    }
+
+}
