41ab5bda5a49e18bb523b476b413c50b8d8db7e2
xueff <657413186@qq.com>
2019-04-19 14:01:34 +0800

    job执行完才能开启 新job

---
+++ src/main/java/jar/quartz/JobTest.java
@@ -0,0 +1,19 @@
+package jar.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class JobTest implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String printTime = new SimpleDateFormat("yy-MM-dd HH-mm-ss").format(new Date());
+        System.out.println("PrintWordsJob start at:" + printTime + ", prints: Hello Job-" + new Random().nextInt(100));
+
+    }
+}
