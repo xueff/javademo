d7e5dd97a1c84bbb7e5704eb9762a7d0eccf0e66
薛飞 <657413186@qq.com>
2019-08-01 21:37:37 +0800

    mapdb

--- init/src/main/java/database/mapdb/MapDbOld.java
+++ init/src/main/java/database/mapdb/MapDbOld.java
@@ -1,86 +1,77 @@
-//package database.mapdb;
-//
-//import org.mapdb.BTreeMap;
-//import org.mapdb.DB;
-//import org.mapdb.DBMaker;
-//
-//import java.io.File;
-//import java.util.Iterator;
-//import java.util.Map;
-//
-///**
-// *         <dependency>
-// *             <groupId>org.mapdb</groupId>
-// *             <artifactId>mapdb</artifactId>
-// *             <version>2.0-beta5</version>
-// *         </dependency>
-// */
-//
-///**
-// * @author xuefei
-// * @description main
-// * @date 2019/7/17
-// */
-//public class MapDbOld {
-//
-//    public static void main(String[] args) {
-//        put();
-////        get();
-//        File file = new File("/opt/data/a.db");
-//        DB db = DBMaker.fileDB(file)
-//                .fileMmapEnable()
-//                .make();
-//        Map<String, Object> monitorDataMap = db.getAll();
-//        Iterator iter = monitorDataMap.keySet().iterator();
-//
-//
+package database.mapdb;
+
+import org.mapdb.BTreeMap;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *         <dependency>
+ *             <groupId>org.mapdb</groupId>
+ *             <artifactId>mapdb</artifactId>
+ *             <version>2.0-beta5</version>
+ *         </dependency>
+ */
+
+/**
+ * @author xuefei
+ * @description main
+ * @date 2019/7/17
+ */
+public class MapDbOld {
+
+    public static void main(String[] args) {
+
+        for(int i=0;i<100;i++) {
+//            put();
+            get();
+        }
+
+
+    }
+
+    private static void get(){
+        File file = new File("/opt/data/a.db");
+        DB db = DBMaker.fileDB(file)/*.closeOnJvmShutdown()*/
+                .fileMmapEnable()
+                .fileLockDisable()
+                .make();
+        Map<String, String> monitorDataMap = db.hashMap("A");
+        Iterator iter = monitorDataMap.keySet().iterator();
+
+        System.out.println(monitorDataMap.size());
 //        while (iter.hasNext()) {
 //
 //            String key = (String) iter.next();
 //
-//            System.out.println(key);
-//            System.out.println(monitorDataMap.get(key));
-//        }
-//
-//        db.close();
-//
-//    }
-//
-//    private static void get(){
-//        File file = new File("/opt/data/a.db");
-//        DB db = DBMaker.fileDB(file)
-//                .fileMmapEnable()
-//                .make();
-//        BTreeMap<String, String> monitorDataMap = db.getTreeMap("monitorDataMap");
-//        Iterator iter = monitorDataMap.keySet().iterator();
-//
-//
-//        while (iter.hasNext()) {
-//
-//            String key = (String) iter.next();
-//
-//            System.out.println(key);
-//        }
-//
-//        db.close();
-//
-//    }
-//
-//    private static void put(){
-//        File file = new File("/opt/data/a.db");
-//        DB db = DBMaker.fileDB(file)
-//                .fileMmapEnable()
-//                .make();
-//        Map  map = db.hashMap("A");
-//
-//        for(int i=0;i<5;i++) {
-//            System.out.println(db.hashMap("A"));
-//
-//            map.put("name"+i, "Young");
 //
 //        }
 //        db.commit();
 //        db.close();
-//
-//    }
-//}
+
+
+    }
+
+    private static void put(){
+        File file = new File("/opt/data/a.db");
+        DB db = DBMaker.fileDB(file)
+                .fileMmapEnable()
+//                .fileLockDisable()
+                .make();
+        Map  map = db.hashMap("A");
+
+        for(int i=0;i<500000;i++) {
+            System.out.println(db.hashMap("A"));
+
+            map.put("name"+i, "Young");
+            db.commit();
+
+        }
+
+        db.close();
+
+    }
+}
--- init/src/test/java/test/Teat.java
+++ init/src/test/java/test/Teat.java
@@ -16,73 +16,13 @@ import java.util.*;
  */
 public class Teat {
     static List<JsonObject> historyList = Collections.synchronizedList(new ArrayList<>());
+
     public static void main(String[] args) {
-        for(int i=0;i<1000;i++) {
+        for (int i = 0; i < 1000; i++) {
 
             HttpClientUtilJson.httpPostRaw("http://localhost:9090/dynamic/policy/distribute/enable", "[24,32,33,34,35,36,37,38,42]", null, null);
 
         }
-
-//        System.out.println("st");
-//        for (int i = 0; i < 5000; i++) {
-//            System.out.println("('192.168.101.203', '2181', 'Simple', '/opt/idss/keytab/1545802006067_', '', '1', '2018-12-26 13:26:46', 'admin"+i+"', NULL, NULL, '192.168.101.203')," );
-////            System.out.println(" ( 'hive"+i+"', 'jdbc:hive2://172.16.0.139:10000/default', '172.16.0.139 ', '10000', 'admindddddd', '!QAZXSW@3', 'Simple', '', '', NULL, '1', '1', '2019-07-15 06:01:52', 'admin', '2019-07-15 06:10:06', 'admin'),");
-//
-//        }
-//        System.out.println("ed");
     }
-
-
-
-
-
-
-
-
-
-
-//
-//        Comparator<JsonObject> comparator = new Comparator<JsonObject>() {
-//            @Override
-//            public int compare(JsonObject o1, JsonObject o2) {
-//                return (int) (o2.getJsonObject("cmd").getLong("TimeStamp") - o1.getJsonObject("cmd").getLong("TimeStamp"));
-//            }
-//        };
-//
-//        for(int i=0;i<300;i++) {
-//            new Thread(new Add()).start();
-//        }
-//        try {
-//            Thread.sleep(8);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(historyList.size());
-//        historyList.sort(comparator.reversed());
-//        System.out.println(historyList);
-//        try {
-//            Thread.sleep(10);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//
-//        System.out.println(historyList.size());
-//    }
-//
-//    private static String subString(String str){
-//        String sb = str.substring(1);
-//        return subString(sb);
-//    }
 }
-
-//class Add implements Runnable{
-//    @Override
-//    public void run() {
-//        for(int i=0;i<100;i++) {
-//            Teat.historyList.add(new JsonObject().put("cmd", new JsonObject().put("TimeStamp", RamdonStudy.getRamdonInt(10002))));
-//        }
-//    }
-//}
 
