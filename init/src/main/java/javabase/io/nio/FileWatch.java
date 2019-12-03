367a98084da79d130668bb1d2c28fbca5862cf60
薛飞 <657413186@qq.com>
2019-12-03 10:40:20 +0800

    filewatch

--- init/src/main/java/javabase/io/nio/FileWatch.java
+++ init/src/main/java/javabase/io/nio/FileWatch.java
@@ -9,7 +9,7 @@ import java.nio.file.*;
  * @author xuefei
  * @Title: ${file_name}1
  * @Package ${package_name}
- * @Description:  获取当前OS平台下的文件系统监控器
+ * @Description:  **Nio WatchService**  获取当前OS平台下的文件系统监控器(文件增删改监控器)
  * @date 2018/10/2914:48
  */
 public class FileWatch {
@@ -18,7 +18,7 @@ public class FileWatch {
     public void fileWatch() throws IOException, InterruptedException {
         WatchService watcher = FileSystems.getDefault().newWatchService();
         //有多字符串时，拼接成一个路径
-        Paths.get("D:\\DC\\Xctrl","xctrl2.0").register(watcher,
+        Paths.get("D:\\push").register(watcher,
                 StandardWatchEventKinds.ENTRY_CREATE,
                 StandardWatchEventKinds.ENTRY_DELETE,
                 StandardWatchEventKinds.ENTRY_MODIFY);
