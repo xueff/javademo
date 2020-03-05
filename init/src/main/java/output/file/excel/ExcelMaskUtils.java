2eef9ac94828151fc05eebe2b3bdd57b6a9b5979
薛飞 <657413186@qq.com>
2020-03-05 15:31:01 +0800

    excel

---
+++ init/src/main/java/output/file/excel/ExcelMaskUtils.java
@@ -0,0 +1,258 @@
+package output.file.excel;
+
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
+import com.idss.processor.watermarkengine.WaterKey;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Created on: 2020/02/26.
+ * Author:     xuefei
+ */
+public class ExcelMaskUtils {
+
+    public static void main(String[] args) {
+        System.out.println(new ExcelMaskUtils().maskExcel("C:\\Users\\admin\\Desktop\\ocyangjiatao\\bb.xlsx","C:\\Users\\admin\\Desktop\\watermark\\ocyangjiatao\\bb.xlsx","11111111000000011101010101010101"));
+        System.out.println(new ExcelMaskUtils().traceExcel("C:\\Users\\admin\\Desktop\\watermark\\ocyangjiatao\\bb.xlsx"));
+    }
+
+    public boolean maskExcel(String path,String fileOutPath,String waterKey) {
+        try {
+            long startTime = System.currentTimeMillis();
+            File file = FileUtil.file(fileOutPath);
+//            if(file.exists()){
+//                file.delete();
+//            }
+            if(!System.getProperty("os.name").toLowerCase().startsWith("win")) {
+                Runtime.getRuntime().exec(" cp -f "+path +" "+fileOutPath);
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("copy:"+(endTime-startTime));
+            if(FileUtil.file(fileOutPath).exists()){
+                ExcelWriter excelWriter = ExcelUtil.getWriter(FileUtil.file(fileOutPath));
+                int rowMax = 1;
+                int columnMax = 1;
+                for (int i = 1; ; i++) {
+                    if (getValue(excelWriter.getCell(i, 1)) == null) {
+                        columnMax = i;
+                        break;
+                    }
+                }
+                startTime = System.currentTimeMillis();
+                System.out.println("列:"+(startTime-endTime));
+                endTime = System.currentTimeMillis();
+                for (int i = 1; ; i++) {
+                    if (getValue(excelWriter.getCell(1, i)) == null) {
+                        rowMax = i;
+                        break;
+                    }
+                }
+                System.out.println("行:"+(endTime-startTime));
+
+                List<String> ChineseList = new WaterKey().getEncryptChineseList(waterKey);
+                String encryptChinese = new WaterKey().getWaterKeyToEncryptChinese(waterKey);
+                System.out.println(WaterKey.getEncryptChineseToWaterKey(encryptChinese));
+                setWaterValueAndWrite(excelWriter, rowMax, columnMax + 20, ChineseList, encryptChinese,fileOutPath);
+            }else {
+                ExcelReader excelReader = ExcelUtil.getReader(FileUtil.file(path),0);
+                int rowMax = 1;
+                int columnMax = 1;
+                for (int i = 1; ; i++) {
+                    if (getValue(excelReader.getCell(i, 1)) == null) {
+                        columnMax = i;
+                        break;
+                    }
+                }
+                startTime = System.currentTimeMillis();
+                System.out.println("列:"+(startTime-endTime));
+                endTime = System.currentTimeMillis();
+                for (int i = 1; ; i++) {
+                    if (getValue(excelReader.getCell(1, i)) == null) {
+                        rowMax = i;
+                        break;
+                    }
+                }
+                System.out.println("行:"+(endTime-startTime));
+                List<String> ChineseList = new WaterKey().getEncryptChineseList(waterKey);
+                String encryptChinese = new WaterKey().getWaterKeyToEncryptChinese(waterKey);
+                System.out.println(WaterKey.getEncryptChineseToWaterKey(encryptChinese));
+                setWaterValueAndWrite(excelReader.getWriter(), rowMax, columnMax + 20, ChineseList, encryptChinese,fileOutPath);
+            }
+
+
+
+
+
+        }catch (Exception e){
+            return  false;
+        }
+        return true;
+    }
+
+    public Map<String,Object> traceExcel(String path) {
+        Map<String,Object> traceMap = new HashMap<>();
+        //模板处理 sheetIndex=0代表第一个
+        ExcelReader readerModel = ExcelUtil.getReader(FileUtil.file(path),0);
+        int rowMax = 1;
+        int columnMax = 1;
+        List<String> chineseList = new ArrayList<>();
+        Ok:
+        for(int i=rowMax;i<500;i++){
+            for(int j=columnMax;j<(columnMax+100);j++) {
+                String value = getValue(readerModel.getCell(j, i));
+                if (StringUtils.isEmpty(value)) {
+                    continue;
+                } else if (isMaskValue(value)) {
+                    chineseList.add(value);
+                    traceMap.put("maskList",chineseList);
+                    getChineseList(readerModel,i,j,traceMap);
+                    break Ok;
+                } else {
+                    columnMax = j+1;
+                }
+            }
+        }
+        return traceMap;
+    }
+
+
+    /**
+     * 向excel写入水印和密钥水印
+     */
+    public void setWaterValueAndWrite(ExcelWriter excelWriter, int row, int column, List<String> list, String encryptChinese, String fileOutPath) {
+        int rowLength = (row<100?100:row);
+        int rowStart = 1;
+        for(int i=0;i<rowLength;i++){
+            for(int j=0;j<list.size();j++){
+                excelWriter.getOrCreateCell(column,rowStart).setCellValue(list.get(j));
+                excelWriter.getOrCreateCell(column+1,rowStart).setCellValue(encryptChinese);
+                rowStart+=1;
+            }
+        }
+        excelWriter.flush(FileUtil.file(fileOutPath));
+
+    }
+    /**
+     * 获取excel cell的值
+     */
+    public String getValue(Cell c) {
+        if(c == null){
+            return null;
+        }
+        try {
+            if(CellType.NUMERIC.equals(c.getCellTypeEnum())) {
+                return c.getNumericCellValue()+"";
+            }else{
+                return c.getStringCellValue();
+            }
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 是否是水印字符
+     */
+    public boolean isMaskValue(String value) {
+        if(StringUtils.isEmpty(value) || value.length()!=8){
+            return false;
+        }
+
+        for (int i = 0; i<value.length(); i++) {
+            String it = Integer.toString((int) value.charAt(i));
+            if(!WaterKey.indexMap2.containsKey(it)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean isEncryptChineseWaterKey(String value) {
+        if(StringUtils.isEmpty(value) || value.length()%8!=0){
+            return false;
+        }
+
+        char first = (char)WaterKey.hideChar[0];
+        char second = (char)WaterKey.hideChar[1];
+        for (int i = 0; i<value.length(); i++) {
+            char it = value.charAt(i);
+            if(it != first && it !=second){
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * 获取excel水印列表
+     */
+
+    private void getChineseList(ExcelReader readerModel, int row, int column, Map<String, Object> traceMap) {
+        List<String> list = (List<String>)traceMap.get("maskList");
+        boolean findaterKey = isChineseWaterKey(readerModel, row, column+1, traceMap);
+        int continueRow = 30;
+        row+=1;
+
+        ok:
+        for (int i = row+1; ; i++) {
+            String value = getValue(readerModel.getCell(column,i));
+            if(isMaskValue(value)){
+                list.add(value);
+                if(!findaterKey){
+                    findaterKey = isChineseWaterKey(readerModel, i, column+1, traceMap);
+                }
+            }else{
+                if(continueRow<=0){
+                    break ok;
+                }
+                for (int j = column-10; j<=column+10; j++) {
+                    value = getValue(readerModel.getCell(column,i));
+                    if(isMaskValue(value)){
+                        list.add(value);
+                        if(!findaterKey){
+                            findaterKey = isChineseWaterKey(readerModel, i, column+1, traceMap);
+                        }
+                        continueRow = 30;
+                        continue ok;
+                    }else{
+                        continue ;
+                    }
+                }
+                continueRow -= continueRow;
+            }
+        }
+    }
+    /**
+     * 水印密钥
+     */
+    private boolean isChineseWaterKey(ExcelReader readerModel, int row, int column, Map<String, Object> traceMap) {
+
+        String EncryptChineseWaterKey = getValue(readerModel.getCell(column,row));
+        if(isEncryptChineseWaterKey(EncryptChineseWaterKey)){
+            WaterKey.getEncryptChineseToWaterKey(EncryptChineseWaterKey);
+            traceMap.put("waterKey", WaterKey.getEncryptChineseToWaterKey(EncryptChineseWaterKey));
+            return true;
+        }
+        return false;
+    }
+
+//
+//    public void  throws Throwable {
+//        //创建基于stream的工作薄对象的
+//        Workbook wb = new SXSSFWorkbook(100); // keep 100 rows in memory, exceeding rows will be flushed to disk
+//        //SXSSFWorkbook wb = new SXSSFWorkbook();
+//        //wb.setCompressTempFiles(true); // temp files will be gzipped
+//
+//    }
+
+}
---
+++ init/src/main/java/output/file/excel/ExcelMaskUtils2.java
@@ -0,0 +1,232 @@
+package output.file.excel;
+
+
+import com.idss.processor.watermarkengine.WaterKey;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.List;
+
+/*
+ * Created on: 2020/02/26.
+ * Author:     xuefei
+ */
+public class ExcelMaskUtils2 {
+    private static Logger log = LoggerFactory.getLogger(ExcelMaskUtils2.class);
+    public static void main(String[] args) {
+
+        new ExcelMaskUtils2().maskExcel("C:\\Users\\admin\\Desktop\\ocyangjiatao\\aa.xlsx","C:\\Users\\admin\\Desktop\\watermark\\ocyangjiatao\\aa.xlsx","11111111000000011101010101010101");
+//        System.out.println(new ExcelMaskUtils2().traceExcel("C:\\Users\\admin\\Desktop\\watermark\\ocyangjiatao\\bb.xlsx"));
+    }
+
+    public boolean maskExcel(String path,String fileOutPath,String waterKey) {
+
+        try {
+            List<String> ChineseList = new WaterKey().getEncryptChineseList(waterKey);
+            String encryptChinese = new WaterKey().getWaterKeyToEncryptChinese(waterKey);
+            log.info("waterKey:"+WaterKey.getEncryptChineseToWaterKey(encryptChinese));
+            File file = new File(path);
+
+
+            if(!file.exists()){
+                log.error("找不到文件");
+                throw new RuntimeException("找不到文件");
+            }
+
+            if(fileOutPath.endsWith(".xlsx")){
+                mask2007PlusExcel(file,ChineseList, encryptChinese,fileOutPath);
+            }else{
+                mask2003Excel(new File(fileOutPath),ChineseList, encryptChinese,fileOutPath);
+            }
+
+            if(new File(fileOutPath).exists()){
+                if(!System.getProperty("os.name").toLowerCase().startsWith("win")) {
+                    Runtime.getRuntime().exec("chmod -R 777 " + fileOutPath);
+                }
+            }else {
+                return false;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+        return true;
+    }
+
+
+    /**
+     * 要求excel版本在2007以上
+     *
+     * @param file 文件信息
+     * @return
+     * @throws Exception
+     */
+    public void mask2007PlusExcel(File file, List<String> list,String encryptChinese,String fileOutPath)  {
+        XSSFWorkbook xwb = null;
+        try {
+            xwb = new XSSFWorkbook(new FileInputStream(file));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("文件读取异常");
+        }
+        // 读取第一张表格内容
+        XSSFSheet sheet = xwb.getSheetAt(0);
+        XSSFRow row = null;
+        int cellWaterMask = 5;  //非数据区
+        for (int i = 1; i <= 5; i++) {
+            row = sheet.getRow(i);
+            if (row == null) {
+                continue;
+            }
+
+            for (int j = cellWaterMask ; j < row.getLastCellNum(); j++) {
+                XSSFCell cell = row.getCell(j);
+                if (cell == null) {
+                    cellWaterMask = j+1;
+                    continue;
+                }
+                if(StringUtils.isEmpty(cell.getRawValue()) && j>cellWaterMask){
+                    cellWaterMask = j;
+                    continue;
+                }
+
+            }
+        }
+        cellWaterMask+=10;
+        for (int i = sheet.getFirstRowNum() ; i < 80; i++) {
+            row = sheet.getRow(i);
+            if(row ==null){
+                row = sheet.createRow(i);
+            }
+            row.createCell(cellWaterMask).setCellValue(list.get(i%list.size()));
+            row.createCell(cellWaterMask+1).setCellValue(encryptChinese);
+
+        }
+        int maxRow = sheet.getLastRowNum();
+        log.info("set waterkey");
+        for (int i = maxRow ; i <= maxRow + 80; i++) {
+            row = sheet.getRow(i);
+            if(row ==null){
+                row = sheet.createRow(i);
+            }
+            row.createCell(cellWaterMask).setCellValue(list.get(i%list.size()));
+            row.createCell(cellWaterMask+1).setCellValue(encryptChinese);
+
+        }
+
+        try {
+            log.info("flush file");
+            FileOutputStream out = new FileOutputStream(fileOutPath);
+            xwb.write(out);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("另一个程序正在使用此文件");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("文件写入失败");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }finally {
+            try {
+                xwb.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.gc();
+        }
+
+    }
+    /**
+     * 要求excel版本在2007以上
+     *
+     * @param file 文件信息
+     * @return
+     * @throws Exception
+     */
+    public void mask2003Excel(File file, List<String> list,String encryptChinese,String fileOutPath) {
+
+        HSSFWorkbook wb = null;
+        try {
+
+            wb = new HSSFWorkbook(new FileInputStream(file));
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            throw new RuntimeException("文件读取异常");
+        }
+        // 读取第一张表格内容
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow row = null;
+
+        // 读取第一张表格内容
+        int cellWaterMask = 5;  //非数据区
+        for (int i = 1; i <= 5; i++) {
+            row = sheet.getRow(i);
+            if (row == null) {
+                continue;
+            }
+
+            for (int j = cellWaterMask ; j < row.getLastCellNum(); j++) {
+                HSSFCell cell = row.getCell(j);
+                if (cell == null) {
+                    cellWaterMask = j+1;
+                    continue;
+                }
+                if(StringUtils.isEmpty(cell.toString()) && j>cellWaterMask){
+                    cellWaterMask = j;
+                    continue;
+                }
+
+            }
+        }
+        cellWaterMask+=10;
+        for (int i = sheet.getFirstRowNum() ; i < 80; i++) {
+            row = sheet.getRow(i);
+            if(row ==null){
+                row = sheet.createRow(i);
+            }
+            row.createCell(cellWaterMask).setCellValue(list.get(i%list.size()));
+            row.createCell(cellWaterMask+1).setCellValue(encryptChinese);
+
+        }
+        int maxRow = sheet.getLastRowNum();
+        log.info("set waterkey");
+        for (int i = maxRow ; i <= maxRow + 80; i++) {
+            row = sheet.getRow(i);
+            if(row ==null){
+                row = sheet.createRow(i);
+            }
+            row.createCell(cellWaterMask).setCellValue(list.get(i%list.size()));
+            row.createCell(cellWaterMask+1).setCellValue(encryptChinese);
+
+        }
+
+        try {
+            log.info("flush file");
+            FileOutputStream out = new FileOutputStream(fileOutPath);
+            wb.write(out);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("文件写入失败");
+        }finally {
+            try {
+                wb.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
