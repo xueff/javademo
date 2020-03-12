c93795b4b42e030e173160051228d0bd02c89c06
薛飞 <657413186@qq.com>
2020-03-12 18:40:11 +0800

    区域找图

---
+++ utils/src/main/java/findpicture/Find.java
@@ -0,0 +1,11 @@
+package findpicture;
+
+public class Find {
+    //使用opencv做模式匹配，在一张图片里面找出跟模板小图片相似的图片出来
+    // https://gitee.com/cheasonxie/matchtemplate?_from=gitee_search
+
+
+
+    //https://gitee.com/PatrickW/image-area-search.git
+    //java实现的区域找图功能
+}
