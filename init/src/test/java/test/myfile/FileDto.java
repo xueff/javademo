6ef9c6b16604d0aa22bb46c665f09aa1433a5479
薛飞 <657413186@qq.com>
2020-09-17 16:26:02 +0800

    书籍

---
+++ init/src/test/java/test/myfile/FileDto.java
@@ -0,0 +1,41 @@
+package test.myfile;
+
+public class FileDto {
+
+    private String path;
+    private String name;
+    private long size;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public FileDto(String path, String name, long size) {
+        this.path = path;
+        this.name = name;
+        this.size = size;
+    }
+
+    public FileDto() {
+    }
+}
---
+++ init/src/test/java/test/myfile/GetFilePath.java
@@ -0,0 +1,47 @@
+package test.myfile;
+
+import javabase.file.excel.ExcelUtils.ExcelUtil;
+import output.file.FileUtils;
+import output.file.file.FindFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class GetFilePath {
+    public static void main(String[] args) throws Exception {
+        List<File> list = FindFile.find("E:\\BaiduNetdiskDownload\\200g",100);
+        List<FileDto> dtos = new ArrayList<>(list.size());
+        Map<String,Long> all = new HashMap<>();
+        Set<String> samesRemove = new HashSet<>();
+        list.forEach(it->{
+            try {
+                if(it.isFile()) {
+                    dtos.add(new FileDto(it.getParentFile().getCanonicalPath(), it.getName(),it.length()));
+                    if(all.containsKey(it.getName())){
+                        if(!all.get(it.getName()).equals(it.length())) {
+                            all.put(it.getName(), it.length());
+                        }else {
+                            samesRemove.add(it.getPath());
+                        }
+                    }else {
+                        all.put(it.getName(), it.length());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        LinkedHashMap<String,String> head = new LinkedHashMap<>();
+        head.put("path","路径");
+        head.put("name","名称");
+        head.put("size","大小");
+        FileOutputStream out = new FileOutputStream("E:\\BaiduNetdiskDownload\\books.xlsx");
+        ExcelUtil.listToExcel(dtos,head,"0",10000,out);
+        System.out.println("samesRemove:"+samesRemove.size());
+        samesRemove.forEach(it->{
+            new File(it).delete();
+        });
+    }
+}
